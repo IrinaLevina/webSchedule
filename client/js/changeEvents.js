14320ae31e4601563bd9762d227355b1b228319f
Irina Levina <irina.levina.i@gmail.com>
2015-06-21 23:24:48 +0300

    change event view
    add new url and new for admin change event view

--- client/js/changeEvents.js
+++ client/js/changeEvents.js
@@ -3,29 +3,43 @@ Template.changeEvents.helpers({
       return Faculties.find({}).fetch();
     },
     courses: function() {
-      Meteor.subscribe('courses')
+      Meteor.subscribe('courses');
       return Session.get('courses')
     },
     groups: function() {
-      Meteor.subscribe('groups')
+      Meteor.subscribe('groups');
        return Session.get('groups')
     },
     events: function() {
-      Meteor.subscribe('events')
-      return Events.find().fetch()
-    },
-  })
+      Meteor.subscribe('events');
+      return Session.get('events')
+    }
+  });
 
 Template.changeEvents.events({
-    'change .faculty-id': function(){
+    'click .faculty-id': function(){
         Session.set('courses', Courses.find({faculty_id: $('.faculty-id').val() }).fetch() );
     },
-    'change .courses-id': function(){
+    'click .courses-id': function(){
         Session.set('groups', Groups.find({course_id: $('.courses-id').val() }).fetch() );
         console.log( $('.courses-id').val());
     },
+    'click .groups-id': function() {
+        Session.set('events', Events.find({group_id: $('.groups-id').val() }).fetch() );
+    },
     'submit .add-event-form': function(e){
         e.preventDefault();
+
+       /* Events.update($('form').attr('data-id'),{$set: {
+            group_id: $('.groups-id').val(),
+            event_name: $('.event-name').val(),
+            event_summury: $('.event-summary').val(),
+            event_date: $('.event-date').val(),
+            event_time: $('.event-time').val(),
+            event_person: $('.event-person').val(),
+            event_place: $('.event-place').val(),
+            event_type: $('.event-type').val()
+        }});*/
         Events.insert({
             group_id: $('.groups-id').val(),
             event_name: $('.event-name').val(),
@@ -36,12 +50,31 @@ Template.changeEvents.events({
             event_place: $('.event-place').val(),
             event_type: $('.event-type').val()
         });
-            $('.event-name').val(""),
-            $('.event-summary').val(""),
-            $('.event-date').val(""),
-            $('.event-time').val(""),
-            $('.event-person').val(""),
-            $('.event-place').val(""),
-            $('.event-type').val("")
+            $('.event-name').val("");
+            $('.event-summary').val("");
+            $('.event-date').val("");
+            $('.event-time').val("");
+            $('.event-person').val("");
+            $('.event-place').val("");
+            $('.event-type').val("");
+        Session.set('events', Events.find({group_id: $('.groups-id').val() }).fetch() );
+    },
+    'click .edit-event':function (e){
+        var currentId = $(e.target).parent('li').attr('data-id');
+        var event = Events.find({"_id": currentId }).fetch();
+        console.log(event);
+        $('.event-name').val(event[0].event_name);
+        $('.event-summary').val(event[0].event_summury);
+        $('.event-date').val(event[0].event_date);
+        $('.event-time').val(event[0].event_time);
+        $('.event-person').val(event[0].event_person);
+        $('.event-place').val(event[0].event_place);
+        $('.event-type').val(event[0].event_type);
+        $('form').attr('data-id', currentId);
+    },
+    'click .delete-event': function(e){
+        var currentId = $(e.target).parent('li').attr('data-id');
+        Events.remove(currentId);
+        Session.set('events', Events.find({group_id: $('.groups-id').val() }).fetch() );
     }
-  })
+  });
