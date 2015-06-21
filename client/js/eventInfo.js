043e1ed9c766359ace81166e0a027fcc3d69e146
Liza <elizaveta.selivanova.eu@gmail.com>
2015-06-22 00:00:38 +0300

    events view
    add type of events on month and days views

--- client/js/eventInfo.js
+++ client/js/eventInfo.js
@@ -4,14 +4,21 @@ Template.eventsOfGroup.events({
             '_id': $(e.target).attr('data-id')
         }).fetch();
         showEventInfo(eventInfo);
-    }
+    },
+	'click .close-button': function(e){
+		hideEventInfo();
+	}
 });
 
 function showEventInfo(eventInfo) {
 //    popup here
 	$(eventInfo).each(function(k,v) {
-		$('.page-popup.event-popup').fadeIn().append('<div class="page-popup-content"><h4>' + v.event_name + '</h4><p>' + v.event_summury + '</p><p>Преподаватель: ' + v.event_person + '</p><p>Время: ' + v.event_time + '</p><p>Место проведения: ' + v.event_place + '</p></div>').addClass('_show')
+		$('.page-popup.event-popup').fadeIn().append('<div class="page-popup-content"><h4>' + v.event_name + '</h4><p>' + v.event_summury + '</p><p>Преподаватель: ' + v.event_person + '</p><p>Время: ' + v.event_time + '</p><p>Место проведения: ' + v.event_place + '</p><div class="default-button close-button">Закрыть</div></div>');
 	});
-	console.log(eventInfo);
+//	console.log(eventInfo);
 
+}
+function hideEventInfo(){
+	$('.page-popup.event-popup').fadeOut();
+	$('.page-popup-content').remove();
 }
