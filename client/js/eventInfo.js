591c7dd988ebb7a5574fe4e7540a069be7a7804e
Liza <elizaveta.selivanova.eu@gmail.com>
2015-06-21 23:31:50 +0300

    events view
    add type of events on month and days views

--- client/js/eventInfo.js
+++ client/js/eventInfo.js
@@ -10,7 +10,7 @@ Template.eventsOfGroup.events({
 function showEventInfo(eventInfo) {
 //    popup here
 	$(eventInfo).each(function(k,v) {
-		$('.page-popup.event-popup').fadeIn().append('<div class="page-popup-content"><div class="page-popup-caption">' + v.event_name + '</div>')
+		$('.page-popup.event-popup').fadeIn().append('<div class="page-popup-content"><h4>' + v.event_name + '</h4><p>' + v.event_summury + '</p><p>Преподаватель: ' + v.event_person + '</p><p>Время: ' + v.event_time + '</p><p>Место проведения: ' + v.event_place + '</p></div>').addClass('_show')
 	});
 	console.log(eventInfo);
 
