59a25d4ef05663a394b8ea648715694e1767ae3d
Irina Levina <irina.levina.i@gmail.com>
2015-06-21 16:26:48 +0300

    events view
    add type of events on month and days views

--- client/js/eventsRender.js
+++ client/js/eventsRender.js
@@ -48,7 +48,9 @@ function eventsRender(startDate, endDate, selector) {
 function createEventForMonth(eventsList) {
     $(".fc-day").find(".fc-day-content").html("");
     $(eventsList).each(function(k,v){
-        $('.fc-day[data-date='+ v.event_date+']').find(".fc-day-content").append("<div data-id='"+ v._id+"'>"+ v.event_name+"</div>");
+        $('.fc-day[data-date='+ v.event_date+']').find(".fc-day-content").append("<div data-id='"+ v._id+"' data-type='"+ v.event_type+"'>"
+            + v.event_name+
+            "</div>");
     });
 }
 
@@ -57,8 +59,8 @@ function createEventForWeek(eventsList) {
     $(eventsList).each(function(k,v){
         var position = $('.fc-agenda-days .fc-widget-content:not(.fc-last)[data-date='+ v.event_date+']').index()*210;
         var timeBlock = checkEventTime(v.event_time);
-        var html = "<div class='event-for-week' data-id='"+ v._id+"'>" +
-            v.event_name +
+        var html = "<div class='event-for-week' data-id='"+ v._id+"' data-type='"+ v.event_type+"'>"
+            + v.event_name +
         "</div>";
         timeBlock.find('.fc-widget-content').html("").append(html);
     });
