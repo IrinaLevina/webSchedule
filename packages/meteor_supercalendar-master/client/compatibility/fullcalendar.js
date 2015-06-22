4ac5dc351152b78098ec0760f229d0a4928bb23f
Irina Levina <irina.levina.i@gmail.com>
2015-06-22 20:47:52 +0300

    events view
    add typehead plugin for teacher search

--- packages/meteor_supercalendar-master/client/compatibility/fullcalendar.js
+++ packages/meteor_supercalendar-master/client/compatibility/fullcalendar.js
@@ -1741,7 +1741,7 @@ function vborders(element) {
 function setMinHeight(element, height) {
 	height = (typeof height == 'number' ? height + 'px' : height);
 	element.each(function(i, _element) {
-		_element.style.cssText += ';min-height:' + height + ';_height:' + height;
+		_element.style.cssText += ';min-height:110px' + ';_height:' + height;
 		// why can't we just use .css() ? i forget
 	});
 }
