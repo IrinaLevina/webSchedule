ef86a67b0c183d4ecd99b9877eba6730943a5c2b
Irina Levina <irina.levina.i@gmail.com>
2015-06-22 17:55:17 +0300

    events view
    hot fixes

--- collections.js
+++ collections.js
@@ -2,4 +2,5 @@ Faculties = new Mongo.Collection("faculties", {idGeneration: 'STRING'});
 Courses = new Mongo.Collection("courses", {idGeneration: 'STRING'});
 Groups = new Mongo.Collection("groups", {idGeneration: 'STRING'});
 Events = new Mongo.Collection("events", {idGeneration: 'STRING'});
+Teachers = new Mongo.Collection("teachers", {idGeneration: 'STRING'});
 /*Calendar = new Mongo.Collection("calendar", {idGeneration: 'STRING'})*/
