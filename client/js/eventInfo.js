9abaeacad1e19cdf921eb0b46ff6081a9d4797d5
Liza <elizaveta.selivanova.eu@gmail.com>
2015-06-22 21:04:41 +0300

    events view
    add type of events on month and days views

--- client/js/eventInfo.js
+++ client/js/eventInfo.js
@@ -13,7 +13,26 @@ Template.eventsOfGroup.events({
 function showEventInfo(eventInfo) {
 //    popup here
 	$(eventInfo).each(function(k,v) {
-		$('.page-popup.event-popup').fadeIn().append('<div class="page-popup-content"><h4>' + v.event_name + '</h4><p>' + v.event_summury + '</p><p>Преподаватель: ' + v.event_person + '</p><p>Время: ' + v.event_time + '</p><p>Место проведения: ' + v.event_place + '</p><div class="default-button close-button">Закрыть</div></div>');
+		var eventType;
+
+		if(v.event_type == 1){
+			eventType = 'Лекция'
+		}else if(v.event_type == 2){
+			eventType = 'Экзамен'
+		}else if(v.event_type == 3){
+			eventType = 'Практическое занятие'
+		}else if(v.event_type == 4){
+			eventType = 'Контрольная работа'
+		}else if(v.event_type == 5){
+			eventType = 'Зачет'
+		}else if(v.event_type == 6){
+			eventType = 'Семинар'
+		}else if(v.event_type == 7){
+			eventType = 'Коллоквиум'
+		}else if(v.event_type == 8){
+			eventType = 'Внеучебное занятие'
+		}
+		$('.page-popup.event-popup').fadeIn().append('<div class="page-popup-content"><h4>' + v.event_name + '</h4><p>' + v.event_summury + '</p><p>Тип: '+eventType+'</p><p>Преподаватель: ' + v.event_person + '</p><p>Время: ' + v.event_time + '</p><p>Место проведения: ' + v.event_place + '</p><div class="default-button close-button">Закрыть</div></div>');
 	});
 //	console.log(eventInfo);
 
